0723eb536135c5c3f99cd84ae263979d0383f17e
mpearc11 <mpearc11@jhu.edu>
2025-07-31 18:02:42 -0400

    Update module3-2streamlit.py

--- module3-2streamlit.py
+++ module3-2streamlit.py
@@ -4,30 +4,33 @@ import streamlit as st
 
 st.title('Module 3-2')
 
-dna_seq = st.text_input('paste your DNA sequence')
-
+dna_seq = st.text_input('paste your dna sequence')
 dna_seq = Seq(dna_seq)
 
-prot_seq = dna_seq.translate()
-tag_seq = Seq("MGSSHHHHHHSSGDYKDDDKLEVLFQGP")
-unprocessed_seq = tag_seq + prot_seq[1:]
-processed_seq = prot_seq[1:]
+if st.button('translate dna sequence')
+    translation = dna_seq.translate()
+    st.write('protein sequence: ' + str(translation))
 
-unprocessed_param = ProteinAnalysis(unprocessed_seq)
-processed_param = ProteinAnalysis(processed_seq)
+unprocessed_seq = st.text_input('paste your unprocessed protein sequence')
 
-if st.button('analyze dna sequence'):
+if st.button('analyze unprocessed sequence'):
+    unprocessed_param = ProteinAnalysis(unprocessed_seq)
     st.subheader('UNPROCESSED')
     st.write('number of amino acids = ' + str(len(unprocessed_seq)))
     st.write('molecular weight = ' + str(unprocessed_param.molecular_weight()))
     st.write('pI = ' + str(unprocessed_param.isoelectric_point()))
     st.write('extinction coefficient = ' + str(unprocessed_param.molar_extinction_coefficient()[1]))
 
-    st.write ()
+processed_seq = st.text_input('paste your processed protein sequence')
 
+if st.button('analyze processed sequence'):
+    processed_param = ProteinAnalysis(processed_seq)
     st.subheader('PROCESSED')
     st.write('number of amino acids = ' + str(len(processed_seq)))
     st.write('molecular weight = ' + str(processed_param.molecular_weight()))
     st.write('pI = ' + str(processed_param.isoelectric_point()))
     st.write('extinction coefficient = ' + str(processed_param.molar_extinction_coefficient()[1]))
 
+#tag_seq = Seq("MGSSHHHHHHSSGDYKDDDKLEVLFQGP")
+#unprocessed_seq = tag_seq + prot_seq[1:]
+#processed_seq = prot_seq[1:]
