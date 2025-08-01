a31562248c40f9f74ca8b4df963dc96b22f495f7
mpearc11 <mpearc11@jhu.edu>
2025-07-31 21:32:43 -0400

    Update module3-2streamlit.py

--- module3-2streamlit.py
+++ module3-2streamlit.py
@@ -11,25 +11,25 @@ if st.button('translate dna sequence'):
     translation = dna_seq.translate()
     st.write('protein sequence: ' + str(translation))
 
-unprocessed_seq = st.text_input('paste your unprocessed protein sequence')
+    unprocessed_seq = st.text_input('paste your unprocessed protein sequence')
 
 if st.button('analyze unprocessed sequence'):
     unprocessed_param = ProteinAnalysis(unprocessed_seq)
     st.subheader('UNPROCESSED')
     st.write('number of amino acids = ' + str(len(unprocessed_seq)))
-    st.write('molecular weight = ' + str(unprocessed_param.molecular_weight()))
+    st.write('molecular weight = ' + str(unprocessed_param.molecular_weight()) + ' daltons')
     st.write('pI = ' + str(unprocessed_param.isoelectric_point()))
-    st.write('extinction coefficient = ' + str(unprocessed_param.molar_extinction_coefficient()[1]))
+    st.write('extinction coefficient = ' + str(unprocessed_param.molar_extinction_coefficient()[1]) + ' M\u00b-1 cm\u00b-1')
 
-processed_seq = st.text_input('paste your processed protein sequence')
+    processed_seq = st.text_input('paste your processed protein sequence')
 
 if st.button('analyze processed sequence'):
     processed_param = ProteinAnalysis(processed_seq)
     st.subheader('PROCESSED')
     st.write('number of amino acids = ' + str(len(processed_seq)))
-    st.write('molecular weight = ' + str(processed_param.molecular_weight()))
+    st.write('molecular weight = ' + str(processed_param.molecular_weight()) + ' daltons')
     st.write('pI = ' + str(processed_param.isoelectric_point()))
-    st.write('extinction coefficient = ' + str(processed_param.molar_extinction_coefficient()[1]))
+    st.write('extinction coefficient = ' + str(processed_param.molar_extinction_coefficient()[1]) + ' M\u00b-1 cm\u00b-1')
 
 #tag_seq = Seq("MGSSHHHHHHSSGDYKDDDKLEVLFQGP")
 #unprocessed_seq = tag_seq + prot_seq[1:]
